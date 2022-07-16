2ab2c6b0d413bb6c3795b9a0b88f839157b833ac
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2022-07-15 23:33:16 -0400

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Jordan Haynes on 7/15/22.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
